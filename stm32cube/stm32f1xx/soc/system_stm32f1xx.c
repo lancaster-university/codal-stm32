4df15bcde42266e713f082a7d13485463e9e2699
Michal Moskal <michal@moskal.me>
2018-11-26 17:03:50 -0800

    Disable vtor

--- stm32cube/stm32f1xx/soc/system_stm32f1xx.c
+++ stm32cube/stm32f1xx/soc/system_stm32f1xx.c
@@ -220,10 +220,11 @@ void SystemInit (void)
   #endif /* DATA_IN_ExtSRAM */
 #endif 
 
+// We rely on bootloader to set these
 #ifdef VECT_TAB_SRAM
-  SCB->VTOR = SRAM_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal SRAM. */
+  //SCB->VTOR = SRAM_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal SRAM. */
 #else
-  SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH. */
+  //SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH. */
 #endif 
 }
 

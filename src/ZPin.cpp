9d5c6e51e2829265048e54b7bdacfc7f92288751
James Devine <j.devine@lancaster.ac.uk>
2019-01-28 12:11:36 +0000

    Init only when used, condition check clean up

--- src/ZPin.cpp
+++ src/ZPin.cpp
@@ -53,7 +53,7 @@ static ZPin *eventPin[16];
 static ADC_HandleTypeDef AdcHandle;
 static bool adcInited = false;
 
-    struct ZEventConfig
+struct ZEventConfig
 {
     CODAL_TIMESTAMP prevPulse;
 };
@@ -98,31 +98,6 @@ ZPin::ZPin(int id, PinNumber name, PinCapability capability) : codal::Pin(id, na
     this->status = 0x00;
 
     this->pwmCfg = NULL;
-
-    if (!adcInited) {
-        // init adc handler
-        __HAL_RCC_ADC1_CLK_ENABLE(); // enable adc clock
-        AdcHandle.Instance = ADC1;
-        
-        AdcHandle.Init.ClockPrescaler        = ADC_CLOCKPRESCALER_PCLK_DIV4;          /* Asynchronous clock mode, input ADC clock not divided */
-        AdcHandle.Init.Resolution            = ADC_RESOLUTION_12B;            /* 12-bit resolution for converted data */
-        AdcHandle.Init.DataAlign             = ADC_DATAALIGN_RIGHT;           /* Right-alignment for converted data */
-        AdcHandle.Init.ScanConvMode          = DISABLE;                       /* Sequencer disabled (ADC conversion on only 1 channel: channel set on rank 1) */
-        AdcHandle.Init.EOCSelection          = DISABLE;                       /* EOC flag picked-up to indicate conversion end */
-        AdcHandle.Init.ContinuousConvMode    = DISABLE;                       /* Continuous mode disabled to have only 1 conversion at each conversion trig */
-        AdcHandle.Init.NbrOfConversion       = 1;                             /* Parameter discarded because sequencer is disabled */
-        AdcHandle.Init.DiscontinuousConvMode = DISABLE;                       /* Parameter discarded because sequencer is disabled */
-        AdcHandle.Init.NbrOfDiscConversion   = 0;                             /* Parameter discarded because sequencer is disabled */
-        AdcHandle.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T1_CC1;   /* Software start to trig the 1st conversion manually, without external event */
-        AdcHandle.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE; /* Parameter discarded because software trigger chosen */
-        AdcHandle.Init.DMAContinuousRequests = DISABLE;                       /* DMA one-shot mode selected (not applied to this example) */
-
-        HAL_ADC_Init(&AdcHandle);
-        if (HAL_ADC_Init(&AdcHandle) != HAL_OK) {
-            CODAL_ASSERT(995);
-        }
-        adcInited = true;
-    }
 }
 
 void ZPin::disconnect()
@@ -361,9 +336,6 @@ int ZPin::setServoValue(int value, int range, int center)
  */
 int ZPin::getAnalogValue()
 {
-    ADC_ChannelConfTypeDef sConfig;
-    // check if this pin has an analogue mode...
-    //    if (!(PIN_CAPABILITY_ANALOG & capability))
     //check if this pin has an analogue mode...
     if(!(PIN_CAPABILITY_ANALOG & capability))
         return DEVICE_NOT_SUPPORTED;
@@ -372,28 +344,56 @@ int ZPin::getAnalogValue()
     uint8_t ch = STM_PIN_CHANNEL(function);
 
     // Move into an analogue input state if necessary.
-    if (!(status & IO_STATUS_ANALOG_IN)){
+    if (!(status & IO_STATUS_ANALOG_IN))
+    {
         disconnect();
+
+        if (!adcInited)
+        {
+             // init adc handler
+            __HAL_RCC_ADC1_CLK_ENABLE(); // enable adc clock
+            AdcHandle.Instance = ADC1;
+
+            AdcHandle.Init.ClockPrescaler        = ADC_CLOCKPRESCALER_PCLK_DIV4;          /* Asynchronous clock mode, input ADC clock not divided */
+            AdcHandle.Init.Resolution            = ADC_RESOLUTION_10B;            /* 12-bit resolution for converted data */
+            AdcHandle.Init.DataAlign             = ADC_DATAALIGN_RIGHT;           /* Right-alignment for converted data */
+            AdcHandle.Init.ScanConvMode          = DISABLE;                       /* Sequencer disabled (ADC conversion on only 1 channel: channel set on rank 1) */
+            AdcHandle.Init.EOCSelection          = DISABLE;                       /* EOC flag picked-up to indicate conversion end */
+            AdcHandle.Init.ContinuousConvMode    = DISABLE;                       /* Continuous mode disabled to have only 1 conversion at each conversion trig */
+            AdcHandle.Init.NbrOfConversion       = 1;                             /* Parameter discarded because sequencer is disabled */
+            AdcHandle.Init.DiscontinuousConvMode = DISABLE;                       /* Parameter discarded because sequencer is disabled */
+            AdcHandle.Init.NbrOfDiscConversion   = 0;                             /* Parameter discarded because sequencer is disabled */
+            AdcHandle.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T1_CC1;   /* Software start to trig the 1st conversion manually, without external event */
+            AdcHandle.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE; /* Parameter discarded because software trigger chosen */
+            AdcHandle.Init.DMAContinuousRequests = DISABLE;                       /* DMA one-shot mode selected (not applied to this example) */
+
+            CODAL_ASSERT(HAL_ADC_Init(&AdcHandle) == HAL_OK);
+            adcInited = true;
+        }
 
         pin_function(name, function);
         status |= IO_STATUS_ANALOG_IN;
     }
+
+    ADC_ChannelConfTypeDef sConfig;
     sConfig.Channel = ch;
     sConfig.Rank = 1;
     sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
     sConfig.Offset = 0;
-    if(HAL_ADC_ConfigChannel(&AdcHandle, &sConfig) != HAL_OK)
-    {
-        return -996;
-    }
+
+    CODAL_ASSERT(HAL_ADC_ConfigChannel(&AdcHandle, &sConfig) == HAL_OK);
+
     //perform a read!
     HAL_ADC_Start(&AdcHandle);
-    if (HAL_ADC_PollForConversion(&AdcHandle, 500) == HAL_OK){
-        int adcvalue = HAL_ADC_GetValue(&AdcHandle);
-        HAL_ADC_Stop(&AdcHandle);
-        return adcvalue/4; // map 4096 to 1024
-    }
-    return -1;
+
+    int adcValue = DEVICE_CANCELLED;
+
+    if (HAL_ADC_PollForConversion(&AdcHandle, 500) == HAL_OK)
+        adcValue = HAL_ADC_GetValue(&AdcHandle);
+
+    HAL_ADC_Stop(&AdcHandle);
+
+    return adcValue;
 }
 
 /**

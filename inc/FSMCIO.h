c9bbe0b16bf70c8c828885bc3959805770b6ef92
Michal Moskal <michal@moskal.me>
2019-05-20 14:42:26 -0700

    Cut out FSMC HAL   - can now compile for F401 and still get FSMCIO

--- inc/FSMCIO.h
+++ inc/FSMCIO.h
@@ -1,32 +1,17 @@
 #ifndef FSMCIO_H
 #define FSMCIO_H
 
-#include "ST7735.h"
-#include "ZPin.h"
-
-#ifdef FSMC_Bank1
+#include "SPI.h"
+#include "Pin.h"
+#include "ScreenIO.h"
 
 namespace codal
 {
 
-class FSMCIO : public ScreenIO
-{
-public:
-    SRAM_HandleTypeDef hsram;
-    DMA_HandleTypeDef hdma;
-    PVoidCallback doneHandler;
-    void *handlerArg;
-
-    FSMCIO(uint32_t flags, PinNumber wr, PinNumber rd);
-    virtual void send(const void *txBuffer, uint32_t txSize);
-    virtual void startSend(const void *txBuffer, uint32_t txSize, PVoidCallback doneHandler,
-                           void *handlerArg);
-};
+ScreenIO *createParallelScreenIO(uint32_t flags, PinNumber wr, PinNumber rd);
 
 } // namespace codal
 
-#define PARALLEL_SCREEN_IO codal::FSMCIO
-
-#endif
+#define CODAL_CREATE_PARALLEL_SCREEN_IO codal::createParallelScreenIO
 
 #endif
